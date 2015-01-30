b9b139403dabc8e9fafe2f8b6a640ed98a754765
Steven Lu <tacticalazn@gmail.com>
2015-01-30 11:24:17 -0500

    formatting data

--- test/coinbase.js
+++ test/coinbase.js
@@ -5,8 +5,9 @@ var coinbase = require('../lib/coinbase');
 
 describe('coinbase', function() {
   it('should get some historical data', function(done) {
-    coinbase.getHistoricRates(20150128, 900, function(err, data) {
+    coinbase.getHistoricRates(20150128, 3600, function(err, data) {
       expect(err).to.be.null;
+      expect(data).to.be.object;
       done();
     })
   });
