a867d43113395e2b71b687c73e1ca29876aace68
Steven Lu <tacticalazn@gmail.com>
2015-02-02 17:57:06 -0500

    rate computation is too fast

--- cron.js
+++ cron.js
@@ -17,8 +17,8 @@ var rates = [
 
 var periods = _.range(8,96,8);
 
-// fast, per 3 seconds
-new CronJob('*/3 * * * * *', function() {
+// fast, per 15 seconds
+new CronJob('*/15 * * * * *', function() {
   async.series([
     function(cb) {
       jobs.create('sync_trades', {}).save(cb);
