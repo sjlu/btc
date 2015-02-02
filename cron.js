8b12909716f1b49f7d22e08935e869a4539c2c55
Steven Lu <tacticalazn@gmail.com>
2015-02-02 11:09:51 -0500

    build more dema grapsh

--- cron.js
+++ cron.js
@@ -18,9 +18,18 @@ schedule.scheduleJob('* * * * *', function() {
 
 schedule.scheduleJob('1,16,31,46 * * * *', function() {
   var rates = [
-    [900, 10, 'dema'],
-    [900, 25, 'dema'],
-    [900, 50, 'dema']
+    [900, 8, 'dema'],
+    [900, 16, 'dema'],
+    [900, 24, 'dema'],
+    [900, 32, 'dema'],
+    [900, 40, 'dema'],
+    [900, 48, 'dema'],
+    [900, 56, 'dema'],
+    [900, 64, 'dema'],
+    [900, 72, 'dema'],
+    [900, 80, 'dema'],
+    [900, 88, 'dema'],
+    [900, 96, 'dema'],
   ];
 
   async.eachSeries(rates, function(rate, cb) {
