2855105381d08d7b1bff069266b5d15e03d3d9eb
Steven Lu <tacticalazn@gmail.com>
2015-02-02 14:40:58 -0500

    better widths and heights

--- public/client/chart/candlestickChart.js
+++ public/client/chart/candlestickChart.js
@@ -8,8 +8,8 @@ client.directive('candlestickChart', function () {
     templateUrl: 'candlestickChart.html',
     link: function($scope, element, attrs){
       var margin = {top: 20, right: 20, bottom: 30, left: 50},
-              width = 960 - margin.left - margin.right,
-              height = 500 - margin.top - margin.bottom;
+      var width = element[0].clientWidth - margin.left - margin.right,
+      var height = element[0].clientHeight 500 - margin.top - margin.bottom;
 
       var parseDate = d3.time.format("%d-%b-%y").parse;
 
