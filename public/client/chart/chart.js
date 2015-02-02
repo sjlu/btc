3a5aaaaf09f5c8ca3f3ae6b44c996a4571942744
Steven Lu <tacticalazn@gmail.com>
2015-02-02 01:14:54 -0500

    fixing charts

--- public/client/chart/chart.js
+++ public/client/chart/chart.js
@@ -9,13 +9,13 @@ client.controller('chart', function($scope, $http) {
     tooltip: {
       mode: "scrubber",
       formatter: function (x, y, series) {
-        return moment(parseInt(x)).fromNow() + ' : ' + y;
+        return series + ' : ' + moment(parseInt(x)).fromNow() + ' : ' + y;
       }
     },
     series: [
-      {y: 'dema-900-50', axis: 'y1', color: 'green', drawDots: true, dotSize: 2},
-      {y: 'dema-900-25', axis: 'y1', color: 'blue', drawDots: true, dotSize: 2},
-      {y: 'dema-900-10', axis: 'y1', color: 'red', drawDots: true, dotSize: 2},
+      {y: 'dema-900-50', axis: 'y1', color: 'red', drawDots: true, dotSize: 2},
+      {y: 'dema-900-25', axis: 'y1', color: 'green', drawDots: true, dotSize: 2},
+      {y: 'dema-900-10', axis: 'y1', color: 'blue', drawDots: true, dotSize: 2},
       {y: 'close', axis: 'y1', color: 'black', drawDots: true, dotSize: 2},
     ],
     stacks: [],
