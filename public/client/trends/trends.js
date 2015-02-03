07f7c1a512b54f1b00983ed77316735fdd905458
Steven Lu <tacticalazn@gmail.com>
2015-02-03 01:55:04 -0500

    fix more time formatting

--- public/client/trends/trends.js
+++ public/client/trends/trends.js
@@ -5,7 +5,7 @@ client.controller('trends', function($scope, $http) {
   $scope.getData = function() {
     $http.get('/api/trends/'+$scope.algo+'-'+$scope.rate+'-8,24,40').success(function(data) {
       $scope.data = _.map(data, function(row) {
-        row.time = moment(row.time).format('dddd, MMMM Do YYYY, h:mm:ss a z');
+        row.time = moment(row.time).format('dd, MM Do YYYY, h:mm:ss a zz');
         return row;
       });
     });
