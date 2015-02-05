0379a531b674ba53c26b59b8bba7204176b94298
Steven Lu <tacticalazn@gmail.com>
2015-02-05 10:12:21 -0500

    divide by 100

--- public/client/trends/trends.js
+++ public/client/trends/trends.js
@@ -4,7 +4,8 @@ client.controller('trends', function($scope, $http) {
   $scope.threshold = 0;
 
   $scope.getData = function() {
-    $http.get('/api/trends/'+$scope.algo+'-'+$scope.rate+'-8,24,40/?threshold='+$scope.threshold).success(function(data) {
+    var threshold = $scope.threshold / 100;
+    $http.get('/api/trends/'+$scope.algo+'-'+$scope.rate+'-8,24,40/?threshold='+threshold).success(function(data) {
       $scope.data = _.map(data, function(row) {
         row.time = moment(row.time).format('ddd, M/D/YY, h:mm a');
         return row;
