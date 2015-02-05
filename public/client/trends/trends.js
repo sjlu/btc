cca8968b14ba85c749444aac474982aaa75d059d
Steven Lu <tacticalazn@gmail.com>
2015-02-05 10:11:31 -0500

    fixing trends

--- public/client/trends/trends.js
+++ public/client/trends/trends.js
@@ -4,7 +4,7 @@ client.controller('trends', function($scope, $http) {
   $scope.threshold = 0;
 
   $scope.getData = function() {
-    $http.get('/api/trends/'+$scope.algo+'-'+$scope.rate+'-8,24,40').success(function(data) {
+    $http.get('/api/trends/'+$scope.algo+'-'+$scope.rate+'-8,24,40/?threshold='+$scope.threshold).success(function(data) {
       $scope.data = _.map(data, function(row) {
         row.time = moment(row.time).format('ddd, M/D/YY, h:mm a');
         return row;
