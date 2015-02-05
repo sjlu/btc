51d305ea335cf266d82052339226dfdcc001abaf
Steven Lu <tacticalazn@gmail.com>
2015-02-05 10:07:53 -0500

    adding threshold

--- routes/api/trends.js
+++ routes/api/trends.js
@@ -4,17 +4,29 @@ var models = require('../../lib/models');
 var async = require('async');
 var _ = require('lodash');
 var moment = require('moment');
+var Sequelize = require('../lib/sequelize');
 
 router.get('/:key', function(req, res, next) {
 
   var start = moment().subtract(req.query.hoursAgo || 6, 'hours').valueOf();
+  var threshold = req.query.threshold;
 
   models.Trend.findAll({
-    where: {
-      key: req.params.key,
-      time: {
-        gte: start
-      }
+    where:
+      Sequelize.and({
+        key: req.params.key,
+        time: {
+          gte: start
+        }
+      }, Sequelize.or({
+        difference: {
+          gte: threshold
+        },
+      }, {
+        difference: {
+          lte: -1*threshold
+        }
+      })
     },
     order: 'time desc'
   }).complete(function(err, trends) {
