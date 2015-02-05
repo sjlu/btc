7b16e737c21fb3bf2fc6019710219625f46dc5b4
Steven Lu <tacticalazn@gmail.com>
2015-02-04 23:07:32 -0500

    limit hours

--- routes/api/trends.js
+++ routes/api/trends.js
@@ -3,11 +3,18 @@ var router = express.Router();
 var models = require('../../lib/models');
 var async = require('async');
 var _ = require('lodash');
+var time = require('../../lib/time');
 
 router.get('/:key', function(req, res, next) {
+
+  var start = moment().subtract(req.query.hoursAgo || 6, 'hours').valueOf();
+
   models.Trend.findAll({
     where: {
-      key: req.params.key
+      key: req.params.key,
+      time: {
+        gte: start
+      }
     },
     order: 'time desc'
   }).complete(function(err, trends) {
